35b901a3744ecfd367de994a4fac22b64f44ada1
Shay <shayphs@gmail.com>
2023-05-02 11:52:23 +0300

    files menu click outside

--- src/app/upload-files/files-menu/files-menu.component.ts
+++ src/app/upload-files/files-menu/files-menu.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
+import { UploadFilesQuery } from '../state/upload-files.query';
+import { Observable } from 'rxjs';
+import { UploadFile } from '../state/upload-file.model';
 
 @Component({
   selector: 'app-files-menu',
@@ -7,8 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilesMenuComponent implements OnInit {
   isOpen: boolean = false;
+  listFiles$!: Observable<UploadFile[]>;
 
-  constructor() {}
+  constructor(
+    private uploadFilesQuery: UploadFilesQuery,
+    private elementRef: ElementRef
+  ) {}
 
-  ngOnInit(): void {}
+  @HostListener('document:click', ['$event'])
+  clickout(event: Event) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.isOpen = false;
+    }
+  }
+
+  ngOnInit(): void {
+    this.listFiles$ = this.uploadFilesQuery.selectAll();
+    // this.uploadFilesQuery.selectAll().subscribe((res) => {
+    //   console.log(res);
+    // });
+  }
 }
